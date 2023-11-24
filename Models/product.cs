ca9aeccda39c78d37de17adabfa1519a4fe51215
unknown <nishirajnath01@gmail.com>
2023-11-25 01:20:26 +0530

    added product model

---
+++ Models/product.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eShop.Models
+{
+    public class product
+    {
+        [Key]
+        public string product_imageUrl { get; set; }
+        public string product_name { get; set;}
+        public string product_description { get; set;}
+        public string product_unitPrice { get; set;} 
+        public string product_category { get; set;}
+        public string product_unitOfMeasurement { get; set;}
+        public int product_id { get; set;}
+        public string product_brand { get; set;}
+        public string product_inStock { get; set;}
+        public string product_expirationDate { get; set; }
+        public int product_UPC { get; set; }
+        public string product_promotion { get; set; }
+
+
+    }
+}
